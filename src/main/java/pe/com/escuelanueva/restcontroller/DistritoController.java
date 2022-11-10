04794f86b7884fecd0210a9cec5864052daf274e
consultora-infoticsolutions <consultora.infoticsolutions@gmail.com>
2022-11-10 12:57:46 -0500

    Update DistritoController.java

--- src/main/java/pe/com/escuelanueva/restcontroller/DistritoController.java
+++ src/main/java/pe/com/escuelanueva/restcontroller/DistritoController.java
@@ -1,9 +1,13 @@
 package pe.com.escuelanueva.restcontroller;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,6 +42,26 @@ public class DistritoController {
     @PostMapping
     public DistritoEntity add(@RequestBody DistritoEntity d){
         return distritoservice.add(d);
+    }
+    
+     @GetMapping("/{id}")
+    public Optional<DistritoEntity> findById(@PathVariable Long id) {
+        return distritoservice.findById(id);
+    }
+
+    
+
+    @PutMapping("/{id}")
+    public DistritoEntity update(@PathVariable long id, @RequestBody DistritoEntity d) {
+        d.setCodigo(id);
+        return distritoservice.update(d);
+    }
+
+    @DeleteMapping("/{id}")
+    public DistritoEntity delete(@PathVariable long id) {
+        DistritoEntity objdistrito = new DistritoEntity();
+        objdistrito.setEstado(false);
+        return distritoservice.delete(DistritoEntity.builder().codigo(id).build());
     }
     
 }
